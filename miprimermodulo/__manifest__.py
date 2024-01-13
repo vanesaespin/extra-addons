91ffe49e8c428e8dae4622093a2298dcbb472bfc
vanesaespin <vespin@ieshlanz.es>
2024-01-13 14:11:06 +0100

    manifest y models

--- miprimermodulo/__manifest__.py
+++ miprimermodulo/__manifest__.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Primer modulo Vanesa",
-
     'summary': """
         Primer modulo SGE""",
-
     'description': """
         Este modulo ha sido creado con scaffolding y esta sincronizado en Github
     """,
-
     'author': "Vanesa Espín",
     'website': "https://www.ieshlanz.es",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Tools',
+    'version': '16.0',  
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "application": True,
+    "installable": True,
 }
--- miprimermodulo/models/models.py
+++ miprimermodulo/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class miprimermodulo(models.Model):
+  _name = 'miprimermodulo.miprimermodulo'
+  _description = 'miprimermodulo.miprimermodulo'
 
-# class miprimermodulo(models.Model):
-#     _name = 'miprimermodulo.miprimermodulo'
-#     _description = 'miprimermodulo.miprimermodulo'
+  name = fields.Char()
+  value = fields.Integer()
+  value2 = fields.Float(compute="_value_pc", store=True)
+  description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+  @api.depends('value')
+  def _value_pc(self):
+    for record in self:
+        record.value2 = float(record.value) / 100
+ 
