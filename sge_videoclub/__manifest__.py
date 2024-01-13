f900de9aced709116899b60cfabb32ccd33ffbb2
vanesaespin <vespin@ieshlanz.es>
2024-01-13 18:32:32 +0100

    Videoclub first commit

---
+++ sge_videoclub/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Videoclub",
+    "version": "16.0",
+    "author": "Vanesa Espín",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": [
+         "security/ir.model.access.csv",
+         "security/videoclub_security.xml",
+        "views/videoclub.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
