63d39786dea8b0a8982add189be0e33d22bdf39c
vanesaespin <vespin@ieshlanz.es>
2024-01-18 21:37:45 +0100

    modulo videoclub

---
+++ sge_videoclub/models/videoclub.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class pelis(models.Model):
+    #atributos 
+    _name = 'videoclub.pelis' 
+    _description = 'Videoclub - pelis'
+    #campos
+    titulo = fields.Char('Titulo', size=30, required=True, help='Nombre de la peli')
+    director = fields.Char('Director', size=30, required=False, help='Director de la peli', default='')
+    clasificacion = fields.Selection([('TP','Todos los Públicos'),('men12','Menores de 12 años'),('may18','Mayores 18 años')], string='Clasificación', default='TP')
+    presupuesto = fields.Integer()
+    fechaestreno = fields.Date()
+    
