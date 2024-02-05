ce5b5c283a033232bf82dc217bd61da6f0a682fb
vanesaespin <vespin@ieshlanz.es>
2024-02-05 18:11:59 +0100

    campos calculados

--- videoclub_sge/models/videoclub.py
+++ videoclub_sge/models/videoclub.py
@@ -7,6 +7,7 @@ class videoclub_pelis(models.Model):
     _description = 'Película'
     #campos
     titulo = fields.Char('Titulo', size=30, required=True, help='Nombre de la peli')
+    _rec_name = 'titulo'
     director = fields.Char('Director', size=30, required=False, help='Director de la peli', default='')
     clasificacion = fields.Selection([('TP','Todos los Públicos'),('men12','Menores de 12 años'),('may18','Mayores 18 años')], string='Clasificación', default='TP')
     presupuesto = fields.Integer()
@@ -14,6 +15,7 @@ class videoclub_pelis(models.Model):
     #Supongamos que se subvenciona el 30% de la película
     subvencionado = fields.Integer(compute='_valor_subvencion') #no inmediato, solo al guardar
     invertido = fields.Integer(compute='_valor_inversion') #queremos verlo de modo inmediato
+    
 
     def _valor_subvencion(self):
         self.subvencionado=self.presupuesto*0.3
@@ -21,6 +23,13 @@ class videoclub_pelis(models.Model):
     @api.depends('presupuesto')
     def _valor_inversion(self):
         self.invertido=self.presupuesto*0.7
+
+    @api.depends('presupuesto')
+    def _suma_total(self):
+        pelis = self.env['pelis'].search([])
+        self.suma=0
+        for peli in pelis:
+            self.suma = self.suma + peli.presupuesto 
 
 
 
