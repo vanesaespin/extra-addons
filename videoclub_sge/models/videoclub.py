0e47c1f027f1fd4c38a5a78b3f26bd5e869c6496
vanesaespin <vespin@ieshlanz.es>
2024-02-05 18:40:48 +0100

    campos calculados

--- videoclub_sge/models/videoclub.py
+++ videoclub_sge/models/videoclub.py
@@ -11,11 +11,12 @@ class videoclub_pelis(models.Model):
     director = fields.Char('Director', size=30, required=False, help='Director de la peli', default='')
     clasificacion = fields.Selection([('TP','Todos los Públicos'),('men12','Menores de 12 años'),('may18','Mayores 18 años')], string='Clasificación', default='TP')
     presupuesto = fields.Integer()
+    recaudado = fields.Integer()
     fechaestreno = fields.Date()
     #Supongamos que se subvenciona el 30% de la película
     subvencionado = fields.Integer(compute='_valor_subvencion') #no inmediato, solo al guardar
     invertido = fields.Integer(compute='_valor_inversion') #queremos verlo de modo inmediato
-    
+    mas_de_millon_euros = fields.Boolean('Más de un millon de euros', compute='_is_millonario', help='Este valor es verdadero si la peli ha costado más de un millon de euros')
 
     def _valor_subvencion(self):
         self.subvencionado=self.presupuesto*0.3
@@ -25,12 +26,8 @@ class videoclub_pelis(models.Model):
         self.invertido=self.presupuesto*0.7
 
     @api.depends('presupuesto')
-    def _suma_total(self):
-        pelis = self.env['pelis'].search([])
-        self.suma=0
-        for peli in pelis:
-            self.suma = self.suma + peli.presupuesto 
-
+    def _is_millonario(self):
+        self.mas_de_millon_euros=(self.presupuesto > 1000000)
 
 
 # class videoclub_sge(models.Model):
